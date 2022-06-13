626556f781479263367ba1e73b8cbcdcc6f9e0f2
Bennekrouf <mohamed.bennekrouf@gmail.com>
2022-06-13 10:40:16 +0200

    Add backend code and tests

--- programs/mysolapp/src/lib.rs
+++ programs/mysolapp/src/lib.rs
@@ -1,15 +1,40 @@
 use anchor_lang::prelude::*;
-
-declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+use anchor_lang::solana_program::entrypoint::ProgramResult;
+declare_id!("5jFPiEbfu1uoo3Dszzf7sgz5VQmfsBzacz7nRzX4U1L");
 
 #[program]
 pub mod mysolapp {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+    pub fn create(ctx: Context<Create>) -> ProgramResult {
+        let base_account = &mut ctx.accounts.base_account;
+        base_account.count = 0;
+        Ok(())
+    }
+
+    pub fn increment(ctx: Context<Increment>) -> ProgramResult {
+        let base_account = &mut ctx.accounts.base_account;
+        base_account.count += 1;
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct Create<'info> {
+    #[account(init, payer = user, space = 16 + 16)]
+    pub base_account: Account<'info, BaseAccount>,
+    #[account(mut)]
+    pub user: Signer<'info>,
+    pub system_program: Program <'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct Increment<'info> {
+    #[account(mut)]
+    pub base_account: Account<'info, BaseAccount>,
+}
+
+#[account]
+pub struct BaseAccount {
+    pub count: u64,
+}
